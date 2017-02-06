db91877608d48e9e6672b6c30b1adeac5f15c852
ayonsn017 <asen6@wisc.edu>
2017-02-06 15:54:16 -0600

    Use fixed seed and lock to generate RandomTrainTest questions.

--- apps/MoleculeEquivalence/algs/RandomTrainTest/RandomTrainTest.py
+++ apps/MoleculeEquivalence/algs/RandomTrainTest/RandomTrainTest.py
@@ -1,7 +1,7 @@
 import time
 import numpy.random
 import next.utils as utils
-from apps.MoleculeEquivalence.algs.Utils import RandomInstanceGenerator, parameters
+from apps.MoleculeEquivalence.algs.Utils import RandomInstanceGenerator, parameters, ParticipantInfo
 import ast
 
 class RandomTrainTest:
@@ -18,8 +18,10 @@ class RandomTrainTest:
     pretest_generator_key = 'pretest_generator'
     training_generator_key = 'training_generator'
     posttest_generator_key = 'posttest_generator'
-    participant_answers_count_dict_key = 'participant_answers_count_dict'
-    participant_question_generator_dict_key = 'participant_question_generator_dict'
+    pretest_seed_key = 'pretest_seed'
+    training_seed_key = 'training_seed'
+    posttest_seed_key = 'posttest_seed_key'
+    participant_info_dict_key = 'participant_info_dict'
     alg_label_key = 'alg_label'
     def initExp(self,butler, pretest_count, training_count, posttest_count, alg_list):
         """
@@ -36,8 +38,8 @@ class RandomTrainTest:
         butler.algorithms.set(key='training_count', value=training_count)
         butler.algorithms.set(key='posttest_count', value=posttest_count)
         butler.algorithms.set(key='num_reported_answers', value=0)  # the number of total questions answered for this algorithm
-        # dictionary to store the number of questions the participant has answered
-        butler.algorithms.set(key=self.participant_answers_count_dict_key, value={}) 
+        # dictionary to store the information for a participant
+        butler.algorithms.set(key=self.participant_info_dict_key, value={})
 
         # converting string to list of dictionaries
         alg_list = ast.literal_eval(alg_list)
@@ -48,12 +50,23 @@ class RandomTrainTest:
                 alg_params = alg_desc
 
         # extract the pretest, training and posttest file names and instantiate the generators
+        '''
         pretest_question_generator = RandomInstanceGenerator.RandomInstanceGenerator(alg_params[self.pretest_file_key])
         butler.algorithms.set(key=self.pretest_generator_key, value=pretest_question_generator)
         training_question_generator = RandomInstanceGenerator.RandomInstanceGenerator(alg_params[self.training_file_key])
         butler.algorithms.set(key=self.training_generator_key, value=training_question_generator)
         posttest_question_generator = RandomInstanceGenerator.RandomInstanceGenerator(alg_params[self.posttest_file_key])
         butler.algorithms.set(key=self.posttest_generator_key, value=posttest_question_generator)
+        '''
+        # save the pretest, training and posttest files
+        butler.algorithms.set(key=self.pretest_file_key, value=alg_params[self.pretest_file_key])
+        butler.algorithms.set(key=self.training_file_key, value=alg_params[self.training_file_key])
+        butler.algorithms.set(key=self.posttest_file_key, value=alg_params[self.posttest_file_key])
+
+        # save the initial seeds
+        butler.algorithms.set(key=self.pretest_seed_key, value=parameters.pretest_seed)
+        butler.algorithms.set(key=self.training_seed_key, value=parameters.training_seed)
+        butler.algorithms.set(key=self.posttest_seed_key, value=parameters.posttest_seed)
 
         return True
 
@@ -67,35 +80,80 @@ class RandomTrainTest:
                     same is 1 if the two molecules are the same 0 otherwise
                     ques_type can currently be pretest, posttest or training
         """
-        # calculate how many question a participant has seen
-        # will decide if the next question would be pretest, training or posttest based on this value
-        participant_answers_count_dict = butler.algorithms.get(key=self.participant_answers_count_dict_key)
-
-        # new participant
-        if participant_uid not in participant_answers_count_dict:
-            participant_answers_count_dict[participant_uid] = 0
-            # need this set step, otherwise butler values are not updated
-            butler.algorithms.set(key=self.participant_answers_count_dict_key, value=participant_answers_count_dict)   
-            num_reported_answers = 0
-        else:
-            num_reported_answers = participant_answers_count_dict[participant_uid]
-
-        # decide which type of question to show and generate that type of question
+        # get the question counts
         pretest_count = butler.algorithms.get(key=self.pretest_count_key)
         training_count = butler.algorithms.get(key=self.training_count_key)
         posttest_count = butler.algorithms.get(key=self.posttest_count_key)
 
+        # acquire the question generation lock
+        ques_gen_lock = butler.memory.lock(parameters.ques_gen_lock_name)
+        ques_gen_lock.acquire()
+
+        # check how many question a participant has seen
+        # will decide if the next question would be pretest, training or posttest based on this value
+        participant_info_dict = butler.algorithms.get(key=self.participant_info_dict_key)
+
+        # new participant
+        if participant_uid not in participant_info_dict:
+            # get the file names
+            pretest_file = butler.algorithms.get(key=self.pretest_file_key)
+            training_file = butler.algorithms.get(key=self.training_file_key)
+            posttest_file = butler.algorithms.get(key=self.posttest_file_key)
+
+            # get the seed for this participant and generate the questions
+            pretest_seed = butler.algorithms.get(key=self.pretest_seed_key)
+            training_seed = butler.algorithms.get(key=self.training_seed_key)
+            posttest_seed = butler.algorithms.get(key=self.posttest_seed_key)
+
+            # generate the questions for this participant and store them
+            pretest_question_generator = RandomInstanceGenerator.RandomInstanceGenerator(pretest_file, seed=pretest_seed)
+            training_question_generator = RandomInstanceGenerator.RandomInstanceGenerator(training_file, seed=training_seed)
+            posttest_question_generator = RandomInstanceGenerator.RandomInstanceGenerator(posttest_file, seed=posttest_seed)
+
+            participant_questions = []
+
+            for i in range(pretest_count):
+                participant_questions.append(pretest_question_generator.generate_question())
+
+            for i in range(training_count):
+                participant_questions.append(training_question_generator.generate_question())
+
+            for i in range(posttest_count):
+                participant_questions.append(posttest_question_generator.generate_question())
+
+            num_reported_answers = 0
+            participant_info = ParticipantInfo.ParticipantInfo(questions=participant_questions, num_reported_answers=num_reported_answers)
+
+            participant_info_dict[participant_uid] = participant_info
+            butler.algorithms.set(key=self.participant_info_dict_key, value=participant_info_dict)
+
+            # increment the seed values
+            butler.algorithms.increment(key=self.pretest_seed_key)
+            butler.algorithms.increment(key=self.training_seed_key)
+            butler.algorithms.increment(key=self.posttest_seed_key)
+
+
+        else:
+            # get the participant information
+            participant_info = butler.algorithms.get(key=self.participant_info_dict_key)[participant_uid]
+            participant_questions = participant_info.questions
+            num_reported_answers = participant_info.num_reported_answers
+        # release the lock
+        ques_gen_lock.release()
+        
+        # decide which type of question to show and generate that type of question
         if num_reported_answers < pretest_count:
-            question_generator = butler.algorithms.get(key=self.pretest_generator_key)
+            #question_generator = butler.algorithms.get(key=self.pretest_generator_key)
             ques_type = self.pretest_key
         elif num_reported_answers >= pretest_count and num_reported_answers < pretest_count + training_count:
-            question_generator = butler.algorithms.get(key=self.training_generator_key)
+            #question_generator = butler.algorithms.get(key=self.training_generator_key)
             ques_type = self.training_key
         elif num_reported_answers >= pretest_count + training_count:
-            question_generator = butler.algorithms.get(key=self.posttest_generator_key)
+            #question_generator = butler.algorithms.get(key=self.posttest_generator_key)
             ques_type = self.posttest_key
 
-        mol1, mol2, same = question_generator.generate_question() 
+        #mol1, mol2, same = question_generator.generate_question() 
+        mol1, mol2, same = participant_questions[num_reported_answers]
 
         return [mol1, mol2, same, ques_type]
 
@@ -108,14 +166,16 @@ class RandomTrainTest:
         butler.algorithms.increment(key='num_reported_answers')
 
         # increment the number of questions the participant has viewed
-        participant_answers_count_dict = butler.algorithms.get(key=self.participant_answers_count_dict_key)
-        num_reported_answers = participant_answers_count_dict[participant_uid] + 1
+        participant_info_dict = butler.algorithms.get(key=self.participant_info_dict_key)
+        participant_info = participant_info_dict[participant_uid]
+        num_reported_answers = participant_info.increment_num_reported_answers()
+
         if num_reported_answers == 12:
-            participant_answers_count_dict.pop(participant_uid, 'None')
+            participant_info_dict.pop(participant_uid, 'None')
         else:
-            participant_answers_count_dict[participant_uid] = num_reported_answers
+            participant_info_dict[participant_uid] = participant_info
         # need this set step, otherwise butler values are not updated
-        butler.algorithms.set(key=self.participant_answers_count_dict_key, value=participant_answers_count_dict)   
+        butler.algorithms.set(key=self.participant_info_dict_key, value=participant_info_dict)   
 
         return True
 
--- apps/MoleculeEquivalence/algs/Utils/parameters.py
+++ apps/MoleculeEquivalence/algs/Utils/parameters.py
@@ -1,3 +1,5 @@
-pretest_seed = 1023
-training_seed = 424
-posttest_seed = 7864
+pretest_seed = 1023  # the starting seed to generate pretest questions
+training_seed = 424  # the starting seed to generate training questions
+posttest_seed = 7864  # the starting seed to generate postest questions
+
+ques_gen_lock_name = 'ques_gen_lock'  # the name of the lock to be used for question generation
