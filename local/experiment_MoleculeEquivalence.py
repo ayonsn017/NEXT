269bfcc4f68c74295910f188fce189df03167019
ayonsn017 <asen6@wisc.edu>
2017-04-03 12:43:05 -0500

    Add Data2 training sequence

--- local/experiment_MoleculeEquivalence.py
+++ local/experiment_MoleculeEquivalence.py
@@ -4,13 +4,15 @@ import os, sys
 sys.path.append("../")
 from launch_experiment import *
 
+# path to files
 target_file = 'data/01_X/mol_img_dict.json'
 pretest_dist_fname = './local/data/02_TestDistribution/test_dist_LewisSF.csv'
 training_dist_fname = './local/data/03_TrainingPool/training_dist_LewisSF.csv'
-training_dataset_fname = './local/data/04_SampleDataset/training_dataset.csv'
-guard_dataset_fname = './local/data/04_SampleDataset/guard_dataset.csv'
+training_dataset_fname = './local/data/04_SampleDataset/training_sequence_Data2.csv'
+guard_dataset_fname = './local/data/04_SampleDataset/guard_questions_Lewis_SF.csv'
 posttest_dist_fname = './local/data/02_TestDistribution/test_dist_LewisSF.csv'
 
+# keys
 pretest_file_key = 'pretest_file'
 training_file_key = 'training_file'
 posttest_file_key = 'posttest_file'
@@ -21,14 +23,14 @@ time_required_key = 'time_required'
 monetary_gain_key = 'monetary_gain'
 
 experiment_list = []
-alg_ids = ['FixedTrainRandomTest','RandomTrainTest']
+alg_ids = ['FixedTrainRandomTest', 'RandomTrainTest']
 
 # Create common alg_list
 alg_list = []
 
 
 # parameters for FixedTrainRandomTest
-alg_item={}
+alg_item = {}
 alg_item[alg_id_key] = alg_ids[0]
 alg_item[alg_label_key] = alg_ids[0]
 alg_item[pretest_file_key] = pretest_dist_fname
@@ -40,7 +42,7 @@ alg_item[monetary_gain_key] = 'You will be entered in a lottery to win a $50 cas
 alg_list.append(alg_item)
 
 # parameters for RandomTrainTest
-alg_item={}
+alg_item = {}
 alg_item[alg_id_key] = alg_ids[1]
 alg_item[alg_label_key] = alg_ids[1]
 alg_item[pretest_file_key] = pretest_dist_fname
@@ -58,7 +60,7 @@ for idx,alg_id in enumerate(alg_ids):
     if idx==0:
         alg_item['alg_label'] = 'Test'
     else:
-        alg_item['alg_label'] = alg_id    
+        alg_item['alg_label'] = alg_id
     alg_item[pretest_file_key] = pretest_dist_fname
     alg_item[training_file_key] = training_dist_fname
     #alg_item[training_file_key] = training_dataset_fname
@@ -68,7 +70,7 @@ for idx,alg_id in enumerate(alg_ids):
     alg_list.append(alg_item)
 '''
 
-# Create common algorithm management settings  
+# Create common algorithm management settings
 params = []
 for algorithm in alg_list:
     params += [{'alg_label': algorithm['alg_label'],
@@ -76,24 +78,24 @@ for algorithm in alg_list:
 
 algorithm_management_settings = {}
 #algorithm_management_settings['mode'] = 'fixed_proportions'
-algorithm_management_settings['mode'] = 'custom'
+algorithm_management_settings['mode'] = 'custom'  # switch between algorithms for each participant
 algorithm_management_settings['params'] = params
 
 
 # Create experiment dictionary
 initExp = {}
 initExp['args'] = {}
-initExp['args']['pretest_count'] = 2
-initExp['args']['training_count'] = 6
-initExp['args']['posttest_count'] = 4
-initExp['args']['guard_gap'] = 5
+initExp['args']['pretest_count'] = 20
+initExp['args']['training_count'] = 60
+initExp['args']['posttest_count'] = 40
+initExp['args']['guard_gap'] = 19
 initExp['args']['participant_to_algorithm_management'] = 'one_to_one'  # assign one participant to one condition only
-initExp['args']['algorithm_management_settings'] = algorithm_management_settings 
-initExp['args']['alg_list'] = alg_list 
-initExp['args']['instructions'] = 'Answer the following question.'
+initExp['args']['algorithm_management_settings'] = algorithm_management_settings
+initExp['args']['alg_list'] = alg_list
+initExp['args']['instructions'] = 'Do not click back or refresh the page!!!'
 initExp['args']['debrief'] = 'Thank you for your participation. Your response has been recorded.'
 # the number of questions the participant will see, this value will be calculated by adding pretest_count, training_count and posttest_count
-initExp['args']['num_tries'] = 1 
+initExp['args']['num_tries'] = 1
 initExp['app_id'] = 'MoleculeEquivalence'
 
 
