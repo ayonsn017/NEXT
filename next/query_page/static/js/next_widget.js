0648252d771afcf093cf3e46da6d2c766e9f97cb
ayonsn017 <asen6@wisc.edu>
2017-11-13 12:49:33 -0600

    Add hack to send query page again

--- next/query_page/static/js/next_widget.js
+++ next/query_page/static/js/next_widget.js
@@ -7,19 +7,20 @@ var next_widget = (function($){
     var _args = null;
     var _callbacks = null;
     var _queryTime = 0;
-    return {	
+    return {
 
-	setUrl : function(url) {      
+	setUrl : function(url) {
 	    _url = url;
 	},
-	
-	getQuery : function(div_id,args,callbacks){
+
+	getQuery : function getQuery(div_id,args,callbacks){
 	    $.ajax({
 		url: _url+"/api/experiment/getQuery",
 		type: "POST",
 		contentType: "application/json",
 		data: JSON.stringify(args),
-		dataType: "json"
+		dataType: "json",
+    timeout: 5000,
 	    }).done( function(data,textStatus, jqXHR) {
 		// Set the div to this html
 		$('#'+div_id).html(data.html);
@@ -28,16 +29,20 @@ var next_widget = (function($){
 		_args = {};
 		_args["exp_uid"] = args["exp_uid"];
 		_args["args"] = {};
-		_args["args"]["query_uid"] = data.args["query_uid"]; 
+		_args["args"]["query_uid"] = data.args["query_uid"];
 		// Set the callbacks
 		_callbacks = callbacks;
 		_callbacks.getQuery_success(data);
 
-	    }).fail( function(error){
+  }).fail( function(error, status){
+    if (status == 'timeout') {
+      getQuery(div_id, args, callbacks);
+      return;
+    }
 		console.log("Failed to get widget data", error);
 		callbacks.widget_failure();
 	    });
-	},	
+	},
 
 	processAnswer: function(args, query_meta) {
 	    $.extend(_args["args"], args);
@@ -56,7 +61,7 @@ var next_widget = (function($){
 		_callbacks.widget_failure();
 	    });
 	},
-	
+
 	shuffle: function(array) {
 	    var currentIndex = array.length, temporaryValue, randomIndex ;
 	    while (0 !== currentIndex) {
@@ -74,12 +79,12 @@ var next_widget = (function($){
 	    var vars = query.split('&');
 	    var pair = {};
 	    for (var i = 0; i < vars.length; i++) {
-		key_val = vars[i].split('='); 
+		key_val = vars[i].split('=');
 		pair[key_val[0]] = key_val[1];
 	    }
 	    return pair;
 	},
-	
+
 	makeRandomString : function(length){
 	    var text = "";
 	    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
