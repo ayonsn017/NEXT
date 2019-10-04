2f8d445cb7c03b9f7ba48b9a9c7ff90212623e97
ayonsn017 <asen6@wisc.edu>
2019-10-04 12:33:57 -0500

    Update RandomInstanceGenerator to send dummy highlight values

--- apps/MoleculeEquivalence/algs/Utils/RandomInstanceGenerator.py
+++ apps/MoleculeEquivalence/algs/Utils/RandomInstanceGenerator.py
@@ -1,3 +1,9 @@
+"""
+script to sample a question from a distribution
+
+@author: Ayon
+"""
+from __future__ import division, print_function
 import numpy as np
 from apps.MoleculeEquivalence.algs.Utils import QuestionGenerator
 
@@ -23,38 +29,49 @@ class RandomInstanceGenerator(QuestionGenerator.QuestionGenerator):
     def generate_question(self):
         """
         return a question generated using the distribution
-        :return: [str, str, int], [representation1 || '_' || molecule1,  representation2 || '_' ||molecule2, same],
-                    same is 1 if the two molecules are the same 0 otherwise
+        :return
+            str, representation1 || '_' || molecule1
+            str, representation2 || '_' || molecule2
+            str, 1 if the two molecules are the same 0 otherwise
+            str, dummy highlight for molecule 1
+            str, dummy highlight for molecule 2
         """
-        multinomial_draw = np.random.multinomial(1, self.probabilities, size=1)[0]
+        multinomial_draw = \
+            np.random.multinomial(1, self.probabilities, size=1)[0]
 
         index = np.where(multinomial_draw == 1)[0][0]
 
-        # generate a RV from Bernoulli distribution to decide whether the molecule positions will be flipped or not
+        # generate a RV from Bernoulli distribution to decide whether the
+        # molecule positions will be flipped or not
         flip = np.random.binomial(1, 0.5)
         if flip == 0:
-            mol1, rep1, mol2, rep2, same = self.distributions[index][:self.probability_index]
+            mol1, rep1, mol2, rep2, same = \
+                self.distributions[index][:self.probability_index]
         else:
-            mol2, rep2, mol1, rep1, same = self.distributions[index][:self.probability_index]
+            mol2, rep2, mol1, rep1, same = \
+                self.distributions[index][:self.probability_index]
 
-        # if this question is found again then generate a new question by recursion
+        # if this question is found again then generate a new question by
+        # recursion
         temp_mol1, temp_mol2 = rep1 + '_' + mol1, rep2 + '_' + mol2
         if temp_mol1 == 'SF_(C2H5)2O' or temp_mol2 == 'SF_(C2H5)2O':
             return self.generate_question()
 
-        return [rep1 + '_' + mol1, rep2 + '_' + mol2, same]
+        return [rep1 + '_' + mol1, rep2 + '_' + mol2, same, '', '']
 
 
 if __name__ == '__main__':
-    input_fname = '../../../../local/data/03_TrainingPool/training_dist_LewisSF.csv'
+    input_fname = \
+        '../../../../local/data/03_TrainingPool/training_dist_LewisSF.csv'
 
     instance_generator1 = RandomInstanceGenerator(input_fname, seed=10)
 
-
     for i in range(6):
-        print i, 'instance generator round 1', instance_generator1.generate_question()
+        print(i, 'instance generator round 1',
+              instance_generator1.generate_question())
 
     np.random.seed(10)
 
     for i in range(6):
-        print i, 'instance generator round 2', instance_generator1.generate_question()
+        print(i, 'instance generator round 2',
+              instance_generator1.generate_question())
